b6d818ef59ca5ffdd1bfea4429b48431f1c643a8
Stefan Davis <stefan@turnkeylinux.org>
2019-06-26 02:09:49 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-fileevent',
+    version='1.0',
+    description='A file object tracking library',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-stdtrap',
+    packages=['fileevent'])
